d7cbb48458348618104faec6cdcd22c7a07f03b5
Jamiz <cjamespardilla@gmail.com>
2025-08-25 14:06:51 +1000

    db configuration updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,5 @@
-// config/db.js
 const mongoose = require("mongoose");
 
-// Set strictQuery explicitly to suppress the warning
 //mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
